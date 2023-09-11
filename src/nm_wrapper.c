92ee5ec2ebfd9e4268f6796bd85dfe98e924f4f8
tsiguenz <tsiguenz@student.42.fr>
2023-09-11 11:30:11 +0200

    print symbols working

--- src/nm_wrapper.c
+++ src/nm_wrapper.c
@@ -13,28 +13,31 @@ void handle_elf64(char *file_name, uint8_t *map, off_t file_size) {
   Elf64_Shdr *shdr = (Elf64_Shdr *)(map + (ehdr->e_shoff));
   char *shstrtab = (char *)&map[shdr[ehdr->e_shstrndx].sh_offset];
   Elf64_Sym *symtab = NULL;
-  Elf64_Dyn *dynsym = NULL;
+  Elf64_Sym *dynsym = NULL;
   uint64_t symtab_size = 0, dynsym_size = 0;
   char *dynstr = NULL, *strtab = NULL;
   (void)dynsym;
   (void)dynstr;
   (void)dynsym_size;
+  (void)symtab;
+  (void)strtab;
+  (void)symtab_size;
 
   for (int i = 0; i < ehdr->e_shnum; i++) {
     const char *section_name = &shstrtab[shdr[i].sh_name];
 
     if (!ft_strncmp(".symtab", section_name, ft_strlen(".symtab")) &&
         shdr[i].sh_type == SHT_SYMTAB) {
-      symtab = (Elf64_Sym *)&shdr[i];
+      symtab = (Elf64_Sym *)&map[shdr[i].sh_offset];
       symtab_size = shdr[i].sh_size;
     }
     if (!ft_strncmp(".strtab", section_name, ft_strlen(".strtab")) &&
         shdr[i].sh_type == SHT_STRTAB) {
       strtab = (char *)&map[shdr[i].sh_offset];
     }
-    if (!ft_strncmp(section_name, ".dynsym", ft_strlen(".dynsym")) &&
+    if (!ft_strncmp(".dynsym", section_name, ft_strlen(".dynsym")) &&
         shdr[i].sh_type == SHT_DYNSYM) {
-      dynsym = (Elf64_Dyn *)&shdr[i];
+      dynsym = (Elf64_Sym *)&map[shdr[i].sh_offset];
       dynsym_size = shdr[i].sh_size;
     }
     if (!ft_strncmp(".dynstr", section_name, ft_strlen(".dynstr")) &&
@@ -43,6 +46,7 @@ void handle_elf64(char *file_name, uint8_t *map, off_t file_size) {
     }
   }
 
+  ft_printf("symtab: \n");
   for (uint64_t i = 0; i < symtab_size / sizeof(Elf64_Sym); i++) {
     char *sym_name = &strtab[symtab[i].st_name];
     if (*sym_name == '\x00')
@@ -50,6 +54,16 @@ void handle_elf64(char *file_name, uint8_t *map, off_t file_size) {
     else
       ft_printf("[%d] %s", i, sym_name);
     ft_printf(" %p\n", symtab[i].st_value);
+  }
+
+  ft_printf("dynsym: \n");
+  for (uint64_t i = 0; i < dynsym_size / sizeof(Elf64_Sym); i++) {
+    char *sym_name = &dynstr[dynsym[i].st_name];
+    if (*sym_name == '\x00')
+      ft_printf("[%d] NULL", i);
+    else
+      ft_printf("[%d] %s", i, sym_name);
+    ft_printf(" %p\n", dynsym[i].st_value);
   }
 }
 
