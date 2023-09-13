e94d53327e57d9d50f9b800185d28db2f98d1e73
tsiguenz <tsiguenz@student.42.fr>
2023-09-13 17:19:24 +0200

    change sorting to match with docker output

--- src/x64/sort.c
+++ src/x64/sort.c
@@ -1,6 +1,6 @@
 #include <ft_nm.h>
 
-void swap_symbol64(t_symbol *a, t_symbol *b) {
+static void swap_symbol(t_symbol *a, t_symbol *b) {
   t_symbol tmp;
 
   tmp.name  = a->name;
@@ -16,29 +16,14 @@ void swap_symbol64(t_symbol *a, t_symbol *b) {
   b->type  = tmp.type;
 }
 
-int symbol_name_compare(char *a, char *b) {
-  while (*a && *b) {
-    while (*a && *a == '_')
-      a++;
-    while (*b && *b == '_')
-      b++;
-    char la = ft_tolower(*a);
-    char lb = ft_tolower(*b);
-    if (la != lb)
-      return la - lb;
-    a++;
-    b++;
-  }
-  return 0;
-}
-
 void sort_default64(t_symbol **lst) {
   if (!lst || !*lst)
     return;
   for (t_symbol *current = *lst; current; current = current->next) {
     for (t_symbol *next = current->next; next; next = next->next) {
-      if (symbol_name_compare(current->name, next->name) > 0)
-        swap_symbol64(current, next);
+      if (ft_strncmp(current->name, next->name, ft_strlen(current->name) + 1) >
+          0)
+        swap_symbol(current, next);
     }
   }
 }
