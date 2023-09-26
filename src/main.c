530bef712b442d23697de18ff2cf25f752a5134d
tsiguenz <tsiguenz@student.42.fr>
2023-09-26 17:07:53 +0200

    delete getopt

--- src/main.c
+++ src/main.c
@@ -5,22 +5,6 @@
 char *prog_name;
 int   multiple_files;
 
-int ft_is_opt(char *arg) { return arg && arg[0] == '-'; }
-
-int ft_getopt(int argc, char **argv, const char *optstring) {
-  static int i = 1;
-  static int j = 1;
-  while (i < argc) {
-    if (argv[i] && ft_is_opt(argv[i]) && argv[i][j]) {
-      j++;
-      return (ft_strchr(optstring, argv[i][j - 1])) ? argv[i][j - 1] : '?';
-    }
-    i++;
-    j = 1;
-  }
-  return -1;
-}
-
 int main(int ac, char **av) {
   prog_name      = av[0];
   multiple_files = ac > 2;
