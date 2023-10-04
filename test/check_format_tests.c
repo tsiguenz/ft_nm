e118df5577c5047562263c41ea09a3887958bcff
tsiguenz <tsiguenz@student.42.fr>
2023-10-04 13:34:14 +0200

    fix ci

--- test/check_format_tests.c
+++ test/check_format_tests.c
@@ -15,18 +15,19 @@ char *check_format_tests(void) {
   {
     char *filename = "test/bin/ok_files/print42_x64";
     test_helper(filename, &fd, &file_size, &map);
-    mu_assert(__FILE__ ": normal elf", !check_format(map, filename));
+    mu_assert(__FILE__ ": normal elf", !check_format(map, filename, file_size));
   }
   {
     char *filename = "test/bin/bad_files/elf_changed_to_ela";
     test_helper(filename, &fd, &file_size, &map);
     mu_assert(__FILE__ ": bad magic numbers (?ELF) elf",
-              check_format(map, filename));
+              check_format(map, filename, file_size));
   }
   {
     char *filename = "test/bin/bad_files/42";
     test_helper(filename, &fd, &file_size, &map);
-    mu_assert(__FILE__ ": file with 2 chars", check_format(map, filename));
+    mu_assert(__FILE__ ": file with 2 chars",
+              check_format(map, filename, file_size));
   }
   return 0;
 }
